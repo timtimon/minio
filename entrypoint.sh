f1c2a22caabf916993f5eaa945bc8f8af11120a9
Krishna Chaitanya <capriciousduck@gmail.com>
2020-11-14 17:14:45 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 cd /minio
 wget --no-check-certificate https://dl.minio.io/server/minio/release/linux-amd64/minio
 chmod +x minio
-./minio server --address :$PORT /mnt/minio/data
+./minio server --address :9000 /mnt/minio/data
